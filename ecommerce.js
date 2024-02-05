59b329da87f90cbbc268cf2f25086490cb5c7275
Mariam-Levy <mariamlevycode@gmail.com>
2024-02-05 20:49:54 -0300

    adjustments and corrections to the navbar

---
+++ ecommerce.js
@@ -0,0 +1,13 @@
+
+const cartIcon = document.getElementById('profile__cart');
+const cartModal = document.getElementById('cart');
+
+cartIcon.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // Verifica si el modal está visible
+    const isModalVisible = window.getComputedStyle(cartModal).display !== 'none';
+
+    // Muestra u oculta el modal
+    cartModal.style.display = isModalVisible ? 'none' : 'block';
+});
